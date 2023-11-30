a0aac7f243d49bd6f9dc990fbb7908ebad80da61
angel <angel_aguado_k2@hotmail.com>
2023-11-29 20:00:16 -0500

    productos

--- clients/src/app/misProductos/page.jsx
+++ clients/src/app/misProductos/page.jsx
@@ -1,46 +1,75 @@
-"use client"
+"use client";
 import React from "react";
-
-import {users} from "./data";
-import { useState ,useEffect} from "react";
-import { useDispatch,useSelector } from "react-redux";
-import {myProducts} from '@/util/http';
-
-
-
-
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+import { users } from "./data";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { myProducts } from "@/util/http";
 
 export default function miProducts() {
-
-   const dispatch=useDispatch()
- const product = useSelector((state)=> state.storeShopping.historyProduct)
- const myProduct = [product]
- console.log(myProduct)
-
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.storeShopping.historyProduct);
+ 
+  console.log(product);
 
   //* conjo el id del localstore
- const userId = JSON.parse(localStorage.getItem('token')).id;
- 
+  const userId = JSON.parse(localStorage.getItem("token")).id;
 
   useEffect(() => {
-   dispatch(myProducts(userId))
-  }, [])
+    dispatch(myProducts(userId));
+  }, []);
+
+  const rows = product.map((product) => ({
+    key: product.id,
+    nombre: product.nombre,
+    categoria: product.categoria,
+    precio: product.precio,
+    tipo: product.tipo,
+  }));
   
 
 
+  const columns = [
+    {
+      key: "nombre",
+      label: "NOMBRE",
+    },
+    {
+      key: "categoria",
+      label: "CATEGORIA",
+    },
+    {
+      key: "precio",
+      label: "PRECIO",
+    },
+    {
+      key: "tipo",
+      label: "Tipo",
+    },
+  ];
+
   return (
-   <div>
-    <div className="bg-gray-700  rounded-lg m-8 border-20 p-8">
-        <div className="bg-white justify p-4 mb-4 rounded-lg mr-4">
-         <p>Nombre</p>
-         <p>precio</p>
-         <p>categoria</p>
-         <p>talla</p>
-         <p>cantidad</p>
-        </div>
-        <div className="bg-white p-4 mb-4">Div 2 (Blanco)</div>
-        <div className="bg-white p-4">Div 3 (Blanco)</div>
-    </div>
-    </div>
+    <Table aria-label="Example table with dynamic content">
+    <TableHeader columns={columns}>
+      {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+    </TableHeader>
+    <TableBody items={rows}>
+      {(item) => (
+        <TableRow key={item.key}>
+          {(columnKey) => (
+            <TableCell>{item[columnKey]}</TableCell>
+          )}
+        </TableRow>
+      )}
+    </TableBody>
+  </Table>
   );
 }
--- clients/src/redux/slice.js
+++ clients/src/redux/slice.js
@@ -52,18 +52,13 @@ export const storelSlice = createSlice({
       state.todalProductos+=1
     },
     setMyProduct:(state,action)=>{
-      state.historyProduct= action.payload;
+      state.historyProduct= [action.payload];
     },
 
   },
 });
-<<<<<<< HEAD
 
 export const { setCarrusel,setProductos ,setOneProductos,setUsuarios ,setHombre,setMujer,setChildren,setGrils,addProductosToCart,setMyProduct} = storelSlice.actions;
-=======
-// console.log([...state.listaProductos,...action.payload])
-export const { setCarrusel,setProductos ,setOneProductos,setUsuarios ,setHombre,setMujer,setChildren,setGrils,addProductosToCart} = storelSlice.actions;
->>>>>>> 3f4047d2bd9b1dc65a67d5f7542ce9b237fd0ba0
 export default storelSlice.reducer;
 
 
